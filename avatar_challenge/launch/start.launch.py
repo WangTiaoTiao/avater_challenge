fe232827ac0e63a6e87940f25091f3a0d319f5c2
default <--default>
2026-03-15 14:49:43 +0000

    v4

--- avatar_challenge/launch/start.launch.py
+++ avatar_challenge/launch/start.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, Node
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
