3ec3b3a6d8242f36bab1e67100bca1c41f5df9b0
WeiWang <Wei@MacBookAir>
2026-03-15 22:43:13 +0800

    v3

--- avatar_challenge/launch/start.launch.py
+++ avatar_challenge/launch/start.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
